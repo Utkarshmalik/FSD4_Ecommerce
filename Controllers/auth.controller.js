152eb2bb05dbfc8cce434247054d812b2f9df3c4
utmalik <utmalik@amazon.com>
2022-09-26 22:48:33 +0530

    login and used async await

--- Controllers/auth.controller.js
+++ Controllers/auth.controller.js
@@ -1,7 +1,8 @@
 const {User, Role , Sequelize, ROLES} = require("../models");
 const bcrypt = require("bcrypt");
 
-exports.signup=(req,res)=>{
+
+exports.signup= async (req,res)=>{
 
     var {userName,email,password,roles}=req.body;
 
@@ -9,27 +10,46 @@ exports.signup=(req,res)=>{
         roles=[ROLES[0]];
     }
 
-    User.create({
-        userName:userName,
-        email:email,
-        password:bcrypt.hashSync(password,8)
-    })
-    .then(user=>{
-            Role.findAll({
-                where:{
-                    name:{
-                        [Sequelize.Op.or] : roles
-                    }
-                }
-            })
-            .then(roles=>{
-                user.setRoles(roles)
-                .then(()=>{
-                    res.send({message:"User resgistered successfully"});
-                })
-            })
-    })
-    .catch((err)=>{
-        res.status(500).send({message:"Something went wrong"});
-    })
+    try{
+    const user = await User.create({userName,email,password:bcrypt.hashSync(password,8)});
+    const userRoles = await  Role.findAll({where:{name:{[Sequelize.Op.or] : roles}}});
+    await  user.setRoles(userRoles);
+    res.send({message:"User resgistered successfully"});
+    }catch(e){
+        res.status(500).send({message: e.message || "Something went wrong" });
+    }
+}
+
+
+
+
+exports.signIn = async (req,res) =>{
+
+    const {username,password}= req.body;
+
+    if(!username || !password){
+        res.status(400).send({message:"UserName or Password cannot be empty"});
+    }
+
+    try{
+    var user = await User.findOne({where:{userName:username}});  
+    }   catch(e){
+        return res.status(500).send({message:e.message});
+    }
+
+    if(!user){
+        return res.status(400).send({message:"user not found"});
+       }
+  
+    var isPasswordValid= bcrypt.compareSync(password,user.password);
+
+    if(!isPasswordValid){
+        return res.status(401).send({message:"Invalid Password"});
+    }
+
+    res.send({id:user.id,
+            userName:user.userName,
+            email:user.email,
+            roles:user.roles
+        })
 }
--- Routes/auth.routes.js
+++ Routes/auth.routes.js
@@ -2,10 +2,11 @@
 const {signUpValidator} = require("../Middlewares");
 const authController = require("../Controllers/auth.controller");
 
-
 module.exports=function(app){    
 
     app.post("/ecomm/api/v1/auth/signup",[signUpValidator.checkDuplicateEmailOrUserName,signUpValidator.checkRolesExists],
     authController.signup);
+
+    app.post("/ecomm/api/v1/auth/signin",authController.signIn);
 
 }
