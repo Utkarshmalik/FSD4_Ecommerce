661eaf8943cdd3884afddbf5703fb6c06127c9a9
utmalik <utmalik@amazon.com>
2022-10-07 21:31:38 +0530

    Cart API's

--- Controllers/cart.controller.js
+++ Controllers/cart.controller.js
@@ -17,15 +17,91 @@ exports.create=async (req,res)=>{
 
 }
 
-exports.findCart= async (req,res)=>{
+exports.update= async (req,res)=>{
 
     const userId=req.user.id;
 
-   const user= await User.findByPk(userId);
+    const user= await User.findByPk(userId);
+ 
+    const cart = await user.getCart();
 
-   const cart = await user.getCart();
+    if(!cart){
+        res.status(400).send({message:"No Cart associated with the user"});
+    }
+
+    const userCart = await Cart.findByPk(cart.id);
+    
+    const newProducts = await Product.findAll({where:{id:req.body.products}});
+
+    if(!newProducts){
+        res.status(400).send({message:"No product exists from the given product Id's"});
+    }
+
+    const existingproducts= await userCart.getProducts();
+
+    const updatedProducts=[...existingproducts, ...newProducts];
+
+    userCart.setProducts(updatedProducts);
+
+    res.send(updatedProducts);
+}
 
 
-   res.send(cart);
+exports.findCart= async (req,res)=>{
+    const {products} = await findCartAndProducts(req.user.id);
 
+    let cost=0;
+
+    for(let i=0;i<products.length;i++){
+        cost+= products[i].cost;
+    }
+
+    res.send({products,totalCost:cost});
+}
+
+
+exports.deleteProductFromCart = async (req,res)=>{
+
+    const productId=parseInt(req.params.id);
+
+    const userId=req.user.id;
+
+    const user= await User.findByPk(userId);
+ 
+    const cart = await user.getCart();
+
+    if(!cart){
+        res.status(400).send({message:"No Cart associated with the user"});
+    }
+
+    const userCart = await Cart.findByPk(cart.id);
+
+    const existingproducts= await userCart.getProducts();
+
+    const updatedProducts = existingproducts.filter((product)=>{
+        return product.id!==productId;
+    })
+
+    await userCart.setProducts(updatedProducts);
+
+    res.send(updatedProducts);
+
+}
+
+
+const findCartAndProducts = async (userId)=>{
+
+    const user= await User.findByPk(userId);
+ 
+    const cart = await user.getCart();
+
+    if(!cart){
+        res.status(400).send({message:"No Cart associated with the user"});
+    }
+
+    const userCart = await Cart.findByPk(cart.id);
+
+    const existingproducts= await userCart.getProducts();
+
+    return  {cart:userCart,products:existingproducts};
 }
--- Routes/cart.routes.js
+++ Routes/cart.routes.js
@@ -5,7 +5,10 @@ module.exports=function(app){
 
     app.post("/ecomm/api/v1/carts",[authJWT.verifyToken],cartControllers.create);
 
+    app.put("/ecomm/api/v1/carts",[authJWT.verifyToken],cartControllers.update);
 
     app.get("/ecomm/api/v1/carts",[authJWT.verifyToken],cartControllers.findCart);
+
+    app.delete("/ecomm/api/v1/carts/products/:id",[authJWT.verifyToken],cartControllers.deleteProductFromCart);
 
 }
